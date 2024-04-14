2f1062c3f7b52aef8b97520f13a6c5d6abb397fe
tomeng70 <tomeng70@users.noreply.github.com>
2024-04-14 07:13:00 -0400

    only displays circle if min or max vals > 0.  also try to center circle about maxLoc.  I think i have to offset it by the bar height (or half the title bar height).

--- track_laser.py
+++ track_laser.py
@@ -2,20 +2,31 @@ import cv2
 import numpy as np
 cap = cv2.VideoCapture(0)
 
+CIRCLE_DIA = 20
+CIRCLE_RAD = 2
+CIRCLE_COLOR = (0, 0, 255)      # rgb
+BAR_HEIGHT = 28
+CIRCLE_OFFSET = BAR_HEIGHT // 2
+
 pts = []
 while (1):
 
     # Take each frame
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    lower_red = np.array([0, 0, 255])
-    upper_red = np.array([255, 255, 255])
+    #lower_red = np.array([170, 50, 50])
+    lower_red = np.array([160, 40, 40])
+    upper_red = np.array([180, 255, 255])
     mask = cv2.inRange(hsv, lower_red, upper_red)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
 
-    cv2.circle(frame, maxLoc, 20, (0, 0, 255), 2, cv2.LINE_AA)
+    if (minVal > 0 or maxVal > 0):
+        print(f"minVal = {minVal:06.2f}, maxVal = {maxVal:06.2f}, " 
+            + f"minLoc = {minLoc}, maxLoc = {maxLoc}")
+        cp = np.add(maxLoc, (0, CIRCLE_OFFSET))
+        cv2.circle(frame, cp, CIRCLE_DIA, CIRCLE_COLOR, CIRCLE_RAD, cv2.LINE_AA)
     cv2.imshow('Track Laser', frame)
+    #cv2.imshow('Track Laser', hsv)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
