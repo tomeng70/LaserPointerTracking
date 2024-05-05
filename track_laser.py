8b69212ce97a94e3a46dffd44fd192de62084cdb
tomeng70 <tomeng70@users.noreply.github.com>
2024-05-05 18:21:38 -0400

    added logic to track first detected pulse of laser and store in array.  instead of printing current location of laser, only display original spot where pulse was first detected.

--- track_laser.py
+++ track_laser.py
@@ -6,8 +6,10 @@ CIRCLE_DIA = 20
 CIRCLE_RAD = 2
 CIRCLE_COLOR = (0, 0, 255)      # rgb
 BAR_HEIGHT = 28
-CIRCLE_OFFSET = BAR_HEIGHT // 2
+CIRCLE_OFFSET = BAR_HEIGHT // 2 - 4
 
+prevState = 0
+currState = 0
 pts = []
 while (1):
 
@@ -15,16 +17,27 @@ while (1):
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #lower_red = np.array([170, 50, 50])
-    lower_red = np.array([160, 40, 40])
+    lower_red = np.array([160, 50, 50])
     upper_red = np.array([180, 255, 255])
     mask = cv2.inRange(hsv, lower_red, upper_red)
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(mask)
 
     if (minVal > 0 or maxVal > 0):
-        print(f"minVal = {minVal:06.2f}, maxVal = {maxVal:06.2f}, " 
-            + f"minLoc = {minLoc}, maxLoc = {maxLoc}")
+        #print(f"minVal = {minVal:06.2f}, maxVal = {maxVal:06.2f}, " 
+        #    + f"minLoc = {minLoc}, maxLoc = {maxLoc}")
         cp = np.add(maxLoc, (0, CIRCLE_OFFSET))
-        cv2.circle(frame, cp, CIRCLE_DIA, CIRCLE_COLOR, CIRCLE_RAD, cv2.LINE_AA)
+        # only add the first point detected in a burst.
+        if (prevState == 0):
+            pts.append(cp)
+            prevState = 1
+        ##cv2.circle(frame, cp, CIRCLE_DIA, CIRCLE_COLOR, CIRCLE_RAD, cv2.LINE_AA)
+    else:
+        if (prevState == 1):
+            prevState = 0
+    # loop through pts.
+    for pt in pts:
+        cv2.circle(frame, pt, CIRCLE_DIA, CIRCLE_COLOR, CIRCLE_RAD, cv2.LINE_AA)
+    
     cv2.imshow('Track Laser', frame)
     #cv2.imshow('Track Laser', hsv)
 
